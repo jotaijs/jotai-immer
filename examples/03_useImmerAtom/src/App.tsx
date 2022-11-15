500da1ec036b2265a0692e0e907ddba9afbad0ab
daishi <daishi@axlight.com>
2022-11-15 18:36:27 +0900

    fix 03_useImmerAtom

--- examples/03_useImmerAtom/src/App.tsx
+++ examples/03_useImmerAtom/src/App.tsx
@@ -1,47 +1,26 @@
-// Bugged.dev
 import React from 'react'
-import { Provider, atom, useAtom } from 'jotai'
-import { atomWithImmer, useImmerAtom, withImmer } from 'jotai-immer'
+import { atom } from 'jotai'
+import { useImmerAtom } from 'jotai/immer'
 
-const numAtom = atom(0) // regular atom
-const derivedNumAtomImmer = withImmer(numAtom) // derived immer atom from regular atom
-const numImmerAtom = atomWithImmer(0) // original immer atom
-const Add = () => {
-  const [derivedNum, setDerivedNum] = useAtom(derivedNumAtomImmer)
-  const [num, setNum] = useAtom(numImmerAtom)
-  const [toImmerNum, setToImmerNum] = useImmerAtom(numAtom) // replace the regular write function with new write function(similar to produce in immer)
-  /*
-  3 ways to have immer:
+const primitiveAtom = atom(0)
 
-    atomWithImmer => useAtom or useImmerAtom
-          Performance -> useAtom > useImmerAtom
-    withImmer => useAtom or useImmerAtom
-          Performace -> useAtom > useImmerAtom
-    atom => useImmerAtom
-          Performance -> useAtom == useImmerAtom
-
-  */
-  return (
-    <div>
-      derived immer atom - number: {derivedNum}
-      <button onClick={() => setDerivedNum((draft) => (draft = draft + 1))}>
-        +
-      </button>
-      <br />
-      original immer atom - number: {num}
-      <button onClick={() => setNum((draft) => (draft = draft + 1))}>+</button>
-      <br />
-      derived immer atom(useImmerAtom): {toImmerNum}
-      <button onClick={() => setToImmerNum((draft) => (draft = draft + 1))}>
-        +
-      </button>
-    </div>
-  )
+const Counter = () => {
+  const [count] = useImmerAtom(primitiveAtom)
+  return <div>count: {count}</div>
 }
+
+const Controls = () => {
+  const [, setCount] = useImmerAtom(primitiveAtom)
+  // setCount === update : (draft: Draft<Value>) => void
+  const inc = () => setCount((c) => (c = c + 1))
+  return <button onClick={inc}>+1</button>
+}
+
 const App = () => (
-  <Provider>
-    <Add />
-  </Provider>
+  <div>
+    <Counter />
+    <Controls />
+  </div>
 )
 
 export default App
