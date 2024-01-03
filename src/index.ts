e6219c77a7ce85c6855c2c20f27279147c02b8f8
Ivan Dramaliev <idrm@users.noreply.github.com>
2024-01-03 13:36:21 +0200

    - add test & example for useSetImmerAtom - export useSetImmerAtom from lib index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { atomWithImmer } from './atomWithImmer'
 export { withImmer } from './withImmer'
 export { useImmerAtom } from './useImmerAtom'
+export { useSetImmerAtom } from './useSetImmerAtom'
